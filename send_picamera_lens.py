79c505f1602e5f9e1033174e7d584a44fe6265d4
japm <josepedreros@gmail.com>
2024-08-27 11:15:18 -0400

    Cambio en send_picamera_lens

--- send_picamera_lens.py
+++ send_picamera_lens.py
@@ -1,15 +1,39 @@
 import cv2
+import sys
 import socket
 import imutils
 import imagezmq
 from picamera2 import Picamera2
 from Lens.liquid_lens_driver import LiquidLensDriver
 import time
-impor
+
+class InvalidVoltageException(Exception):
+    "Raised when the input voltage is not between 0 and 255"
+    pass
+
+class InvalidInputsException(Exception):
+    "Raised when the input number is less of two (voltage  address)"
+    pass
+
+try:
+    if (len(sys.argv[1]) <= 2):
+        raise InvalidInputsException
+    
+except InvalidInputsException:
+    sys.exit("wrong number of inputs, test")
+
+except InvalidVoltageException:
+    sys.exit("Exception occurred: Invalid Voltage, it must be between 0 and 255")
+    
+try:
+    if (int(sys.argv[1]) >= 0) and (int(sys.argv[1]) <=255):
+        voltage = int(sys.argv[1])
+    else:
+        raise InvalidVoltageException
 
 lens = LiquidLensDriver()
 picam2 = Picamera2()
-lens.d_write(0)
+lens.d_write(voltage)
 # (640 ,480)
 # picam2.configure(picam2.create_preview_configuration(main={"format": 'RGB888', "size": (1920, 1080)}, lores={"size" : (320, 240)}, display="lores"))
 # picam2.configure(picam2.create_preview_configuration(raw={"size":(1640,1232)},main={"size": (640, 480)}))
@@ -19,7 +43,7 @@ picam2.configure(picam2.create_video_configuration(raw={'size':fullres},main={'f
 picam2.start()
 
 # socket zmq
-sender = imagezmq.ImageSender(connect_to='tcp://192.168.31.120:5555')
+sender = imagezmq.ImageSender(connect_to='tcp://192.168.31.121:5555')
 rpi_name = socket.gethostname()
 print(rpi_name)
 print(type(rpi_name))
