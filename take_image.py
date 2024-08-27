ab86fb397f3623cd424fb405d7432a2639dbc86a
japm <josepedreros@gmail.com>
2024-08-27 16:57:24 -0400

    improve take_image.py adding resize image

--- take_image.py
+++ take_image.py
@@ -6,9 +6,13 @@ import argparse
 parser = argparse.ArgumentParser(description='Take Images with liquid lens')
 parser.add_argument('-v', '--voltage',nargs='?',const=172, type = int,default=172)  
 parser.add_argument('-x','--xsize',nargs='?', const=3280, type = int, default=3280)
-parser.add_argument('-y','--ysize',nargs='?', const=3280, type = int, default=2464)
-
+parser.add_argument('-y','--ysize',nargs='?', const=2464, type = int, default=2464)
+parser.add_argument('-r','--resize',nargs='?', const=1, type = float, default=1)
 args = parser.parse_args()
+
+# image parameters
+fullres = (args.xsize,args.ysize)
+outres = (int(args.resize*args.xsize),int(args.resize*args.ysize))
 
 try:
     if (int(args.voltage) >= 0) and (int(args.voltage) <=255):
@@ -30,10 +34,15 @@ length = len(files)
 lens = LiquidLensDriver()
 lens.d_write(voltage)
 picam2 = Picamera2()
-camera_config = picam2.create_still_configuration(main={"size":(args.xsize,args.ysize)})
+camera_config = picam2.create_still_configuration(main={"size":fullres})
 picam2.configure(camera_config)
 
 picam2.start()
 time.sleep(2)
-picam2.capture_file(f"{path}/Liquidlens_voltage{voltage:d}_{length+1:0>3.0f}.jpg")
+array = picam2.switch_mode_and_capture_array(camera_config)
+if args.resize <> 1:
+    array = cv2.resize(array, outres)
+    
+cv2.imwrite(f"{path}/Liquidlens_voltage{voltage:d}_{length+1:0>3.0f}.jpg", array)
+# picam2.capture_file(f"{path}/Liquidlens_voltage{voltage:d}_{length+1:0>3.0f}.jpg")
 print(f"Image name:Liquidlens_voltage{voltage:d}_{length+1:0>3.0f}.jpg")
