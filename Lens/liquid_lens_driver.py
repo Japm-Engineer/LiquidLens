f0c275b59f915d3f91e8c19e0e83691cb1c7b0de
jamp <josepedreros@gmail.com>
2024-10-08 17:11:11 -0300

    agregado lens.py para modificar solo voltaje lente liquido, y cambiada direccion a 23. directo al command

--- Lens/liquid_lens_driver.py
+++ Lens/liquid_lens_driver.py
@@ -1,11 +1,11 @@
 import smbus
 addrs = 0b0010111
-# print(addrs)
+# print(0b0010111)
 class LiquidLensDriver:
   def loadDriver(self):
       self.driver = smbus.SMBus(1)
-    #   self.driver.write_byte(addrs, 0x00)
-      self.driver.write_byte(addrs, 0x00)
+    #   self.driver.write_byte(0b0010111, 0x00)
+      self.driver.write_byte(0b0010111, 0x00)
       print("Bus ready: ", self.driver)
 
   def __init__(self, initial=172):
@@ -18,13 +18,13 @@ class LiquidLensDriver:
       # global middlevoltage
 
       if close:
-          self.driver.write_byte(addrs, 0x00)  # Shutdown code
+          self.driver.write_byte(0b0010111, 0x00)  # Shutdown code
           return
 
       if voltage:
-          self.driver.write_byte(addrs, int(voltage) % 256)
+          self.driver.write_byte(0b0010111, int(voltage) % 256)
       else:
-          self.driver.write_byte(addrs, int(self.middlevoltage) % 256)
+          self.driver.write_byte(0b0010111, int(self.middlevoltage) % 256)
 
   def set_voltage(self, volt=False, shutdown=False):
       self.d_write(voltage=volt, close=shutdown)
