6ae593311e9c009a2fd339d30465cbc1642f1b76
Jose Pedreros <josepedreros@gmail.com>
2025-05-09 14:56:10 -0400

    repair np.arrange

--- image_libcamera.py
+++ image_libcamera.py
@@ -29,7 +29,7 @@ if (pathday in os.listdir(pathground)) == False:
     
 if args.text == "":
     path = pathground + pathday +'/'
-voltage = np.arange(1,255,int(args.number))
+voltage = np.arange(1,255,round(255/int(args.number)))
 print(voltage)
 # for v in voltage:
 #     file0 = path + f"cam0_g{args.gain}_exp{args.exposure}_voltage{v}_"+ str(1).zfill(3)+".png"
